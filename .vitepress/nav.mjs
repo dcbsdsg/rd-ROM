a8fdc2e038176804cb6da3509fd6b16ae9d27968
dcbsdsg <1136219593@qq.com>
2025-08-03 01:17:37 +0800

    更新

--- .vitepress/nav.mjs
+++ .vitepress/nav.mjs
@@ -3,12 +3,12 @@ export default [
   {
     text: "配置管理器",
     items: [
-      { text: "使用说明", link: "/configManage/使用说明" },
-      { text: "配置说明", link: "/configManage/配置说明" },
+      { text: "使用说明", link: "/configManage/useGuide/使用说明" },
+      { text: "配置说明", link: "/configManage/configGuide/配置说明" },
     ],
   },
   {
     text: "脚本功能",
-    items: [{ text: "功能说明", link: "/scriptFunction/功能说明" }],
+    items: [{ text: "功能说明", link: "/scriptFunction/funGuide/功能说明" }],
   },
 ];
